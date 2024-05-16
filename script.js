7d7365b1c3c3daadce416cc8a217c66495fbfddd
Astronomy487 <astronomy487@gmail.com>
2024-05-16 01:33:04 -0500

    2 new blog articles
    talking about planck units (Really my first first draft of this dates back to june 2021), and also talking about toki ma the conlang

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ try {
 
 //css
 add_head_link("stylesheet", prefix+"styles.css");
-add_head_link("icon", prefix+"icon.png");
+add_head_link("icon", "icon.png");
 
 //helper functions
 function add_script(url) {
@@ -56,66 +56,34 @@ if (prefix != "" && false) {
 }
 
 
-//custom : graph1
-for (let div of document.querySelectorAll("graph1")) {
-	let [xmin, xmax, ymin, ymax, width, height] = ["xmin", "xmax", "ymin", "ymax", "width", "height"].map(x => parseFloat(div.getAttribute(x)));
-	width = 768;
-	let transformations = eval(div.getAttribute("lambda"));
-	if (typeof transformations == "function") transformations = [transformations];
-	let colors = div.hasAttribute("colors") ? div.getAttribute("colors").split(",") : [];
-	let canvas = div.appendChild(document.createElement("canvas"));
-	canvas.setAttribute("width", width);
-	canvas.setAttribute("height", height);
-	div.style.width = width;
-	div.style.height = height;
-	let context = canvas.getContext("2d");
-	context.lineWidth = 2;
-	
-	//do the axes
-  context.strokeStyle = "#ddd";
-	context.beginPath();
-	context.stroke();
-	
-	for (let i = 0; i < transformations.length; i++) {
-		//do the curve
-		if (i < colors.length) context.strokeStyle = colors[i]
-		else context.strokeStyle = "#fff";
-		context.beginPath();
-		for (let x = 0; x < width; x++) {
-			let t = x/(width-1); //0 <= t <= 1
-			let f = transformations[i](t*(xmax-xmin)+xmin); //f according to function output space
-			//f -> y : ymax -> 0 , ymin -> height-1
-			let outt = 1 - (f - ymin) / (ymax - ymin); //0 for biggest output, 1 for smallest output
-			context.lineTo(x, outt*(height-1));
-		}
-		context.stroke();
-	}
-}
 
-let nav = document.body.appendChild(document.createElement("nav"));
-for (let h2 of document.querySelectorAll("h2")) {
-	let i = h2.innerText.toLowerCase().replaceAll(" ", "-").replace(/[^a-z0-9\-]/gi, '');
-	h2.setAttribute("id", i);
-	let a = nav.appendChild(document.createElement("a"));
-	a.innerText = h2.innerText;
-	a.setAttribute("href", "#" + i);
-	a.onclick = function() {
-		h2.setAttribute("glow", "false");
-		setTimeout(function(){
-			h2.setAttribute("glow", "true");
-		}, 10);
-	}
-}
+
 try {if (isMainPage) {} } catch(e) {
-	let aa = nav.appendChild(document.createElement("a"));
-	aa.innerText = "my blog [where i talk about math]";
-	aa.setAttribute("nobefore", "true");
-	aa.setAttribute("href", "../");
+	let a = document.createElement("a");
+	document.body.insertBefore(a, document.body.firstChild);
+	a.innerText = "blog homepage";
+	a.setAttribute("nobefore", "true");
+	a.setAttribute("href", "../");
 }
-nav.style.opacity = 0;
 
-
-setTimeout(function() {
-	if (nav.getBoundingClientRect().right > document.body.getBoundingClientRect().left) nav.remove();
-	else nav.style.opacity = 1;
-}, 1000);
+let modalOpen = null;
+function hiModal(el) {
+	if (el.getAttribute("transitionbusy") == "true") return;
+	modalOpen = el;
+	el.style.animation = "0.5s modal-in";
+	el.style.display = "block";
+	el.setAttribute("transitionbusy", "true");
+	setTimeout(function() {
+		el.setAttribute("transitionbusy", "false");
+	}, 500)
+}
+function byeModal(el) {
+	if (el.getAttribute("transitionbusy") == "true") return;
+	modalOpen = null;
+	el.style.animation = "0.5s modal-out";
+	el.setAttribute("transitionbusy", "true");
+	setTimeout(function() {
+		el.setAttribute("transitionbusy", "false");
+		el.style.display = "none";
+	}, 500)
+}
